0f1c9d7603e9128849a5325d1b2ac1b828af4cb7
Axolopment <alvaro.tozco.95@gmail.com>
2025-05-27 19:05:34 -0600

    Personalizando la invitacion

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('.cuentaRegresiva', {
 	year: 2025, // required
-	month: 11, // required
-	day: 2, // required
-	hours: 20, // Default is 0 [0-23] integer
+	month: 10, // required
+	day: 25, // required
+	hours: 17, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
